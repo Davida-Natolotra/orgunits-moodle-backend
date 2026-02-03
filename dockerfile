e2785d4b2c7525b0e8f886f18a21a05d297b62a5
Davida <r.davida.natolotra@gmail.com>
2026-02-03 11:17:48 +0300

    mety new cloud gcp

--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
-FROM node:22
+FROM node:22 
 WORKDIR /usr/src/app
-COPY . .
+COPY package*.json ./
 RUN npm install
 COPY . .
 RUN npm run build
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { time } from 'console';
-import { timeout } from 'rxjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Configure CORS
   app.enableCors({
-    origin: 'http://localhost:4200', // Allow requests from your Angular app
+    origin: ['http://localhost:4200', 'https://dave-teck.web.app'], // Allow requests from your Angular app
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allow specific methods
     credentials: true, // Allow authentication cookies/tokens if needed
   });
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
-try {
-  bootstrap();
-  sleep(5000);
-  function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-} catch (error) {
-  console.error(error);
-}
+bootstrap();
